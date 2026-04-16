7328b655c4bfe5370a0670c7258e38ca69bd2def
Supriya gurung <grgsupriyaa890@gmail.com>
2026-04-16 14:57:23 -0500

    updated the dashboard page with analytic section

--- frontend/app/dashboardPpt/page.tsx
+++ frontend/app/dashboardPpt/page.tsx
@@ -1,96 +1,85 @@
 "use client";
+
 import Image from "next/image";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import WinRateChart from "@/components/stats/WinRateChart";
-import CollapsiblePanel from "@/components/ui/CollapsiblePanel";
-//for avatars
+
+// avatars
 import brandonAvatar from "@/public/avatars/brandon.webp";
 import sydneyAvatar from "@/public/avatars/sydney.webp";
 import jenniferAvatar from "@/public/avatars/jennifer.webp";
 import supriyaAvatar from "@/public/avatars/supriya.webp";
+import { StaticImageData } from "next/image";
 
-import { StaticImageData} from "next/image";
-
-//for game images
+// game images
 import terraformImg from "@/public/images/terraforming.webp";
 import catanImg from "@/public/images/catan.webp";
 import everdellImg from "@/public/images/everdell.webp";
 import botanyImg from "@/public/images/botany.webp";
 
-export default function Page() {
-  type Game = {
-    name: string;
-    img: string;
-  };
+type BaseGame = {
+  gameName: string;
+  gameImg: string | StaticImageData;
+ };
 
-  type Play = {
-    name: string;
-    img: string;
-    result: "Won" | "Lost";
-    players: number;
-    time: string;
-    date: string;
-  };
+ type GameResult = BaseGame & {
+  gameResult: "Won" | "Lost";
+  //TODO: extend to include score or draw in future
+ };
 
-  type CommunityItem = {
-  name: string; // game name
-  img: string | StaticImageData; // game image
+ type Game = BaseGame;
+
+type Play = GameResult & {
+  playerCount: number;
+  playDuration: string;
+  playDate: string;
+ };
+
+type CommunityItem = GameResult & {
   user: string;
-  time: string;
-  result: "Won" | "Lost";
+  timeAgo: string;
   avatar?: string | StaticImageData;
-};
+ }; 
 
+const days: string[] = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+export default function Page() {
+  const [openChart, setOpenChart] = useState<null | "winrate" | "donut">(null);
+  // state for avatar to fix hydration
+  const [userAvatar, setUserAvatar] = useState<string | null>(null);
+  useEffect(() => {
+    const avatar = localStorage.getItem("userAvatar");
+    if (avatar) setUserAvatar(avatar);
+  }, []);
+
+  const scrollToAnalytics = () => {
+    document.getElementById("analytics")?.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  };
   const games: Game[] = [
-    { name: "Botany", img: "/images/botany.webp" },
-    { name: "Stardew Valley", img: "/images/stardew.webp" },
-    { name: "Pandemic", img: "/images/pandemic.webp" },
-    { name: "Root", img: "/images/root.webp" },
-    { name: "Catan", img: "/images/catan.webp"},
-  ];
+    { gameName: "Botany", gameImg: "/images/botany.webp" },
+    { gameName: "Stardew Valley", gameImg: "/images/stardew.webp" },
+    { gameName: "Pandemic", gameImg: "/images/pandemic.webp" },
+    { gameName: "Root", gameImg: "/images/root.webp" },
+    { gameName: "Catan", gameImg: "/images/catan.webp" },
+   ];
 
   const recentPlays: Play[] = [
-    {
-      name: "Terraforming Mars",
-      img: "/images/terraforming.webp",
-      result: "Won",
-      players: 2,
-      time: "90 min",
-      date: "Feb 22",
-    },
-    {
-      name: "Catan",
-      img: "/images/catan.webp",
-      result: "Lost",
-      players: 4,
-      time: "60 min",
-      date: "March 11",
-    },
-    {
-      name: "Azul",
-      img: "/images/azul.webp",
-      result: "Won",
-      players: 3,
-      time: "30 min",
-      date: "January 21",
-    },
-  ];
+    { gameName: "Terraforming Mars", gameImg: "/images/terraforming.webp", gameResult:"Won", playerCount: 2, playDuration: "90 min", playDate: "Feb 22" },
+    { gameName: "Catan", gameImg: "/images/catan.webp", gameResult: "Lost", playerCount: 4, playDuration: "60 min", playDate: "March 11" },
+    { gameName: "Azul", gameImg: "/images/azul.webp", gameResult: "Won", playerCount: 3, playDuration: "30 min", playDate: "January 21" },
+   ];
 
- const community: CommunityItem[] = [
-  { user: "Brandon", time: "2hrs ago", result: "Won", avatar: brandonAvatar, name: "Terraforming Mars", img: terraformImg },
-  { user: "Sydney", time: "5hrs ago", result: "Lost", avatar: sydneyAvatar, name: "Catan", img: catanImg },
-  { user: "Jennifer", time: "1d ago", result: "Won", avatar: jenniferAvatar, name: "Everdell", img: everdellImg },
-  { user: "Supriya", time: "2d ago", result: "Lost", avatar: supriyaAvatar, name: "Botany", img: botanyImg },
-];
+  const community: CommunityItem[] = [
+    { user: "Brandon", timeAgo: "2hrs ago", gameResult: "Won", avatar: brandonAvatar, gameName: "Terraforming Mars", gameImg: terraformImg },
+    { user: "Sydney", timeAgo: "5hrs ago", gameResult: "Lost", avatar: sydneyAvatar, gameName: "Catan", gameImg: catanImg },
+    { user: "Jennifer", timeAgo: "1d ago", gameResult: "Won", avatar: jenniferAvatar, gameName: "Everdell", gameImg: everdellImg },
+    { user: "Supriya", timeAgo: "2d ago", gameResult: "Lost", avatar: supriyaAvatar, gameName: "Botany", gameImg: botanyImg },
+   ];
 
-  const days: string[] = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-   
-  const [openChart, setOpenChart] = useState<null | "winrate" | "donut">(null);
-  
-  // get the avatar saved by the user in Avatar Picker
-const userAvatar =
-  typeof window !== "undefined" ? localStorage.getItem("userAvatar") : null;
 
   return (
     <div className=" min-h-screen bg-[#0F172A] text-[#F8FAFC]">
@@ -107,9 +96,17 @@ const userAvatar =
             Recommendations for you
           </p>
         </Link>
+        <Link href="/community">
           <p className="hover:text-white cursor-pointer transition">
             Weeple
           </p>
+        </Link>
+        
+          <Link href="/analytics">
+            <p className="hover:text-white cursor-pointer transition">
+              Your Analytics
+            </p>
+          </Link>
         </div>
 
         <div className="flex items-center gap-3">
@@ -128,7 +125,7 @@ const userAvatar =
     
       <div className="p-8">
         {/* TITLE */}
-        <h1 className="text-3xl font-bold">Welcome to QuestLog</h1>
+        <h1 className="text-4xl font-extrabold tracking-tight text-white">Welcome to QuestLog</h1>
         <p className="text-gray-400 mb-6">Your shared activity hub</p>
 
         {/* TOP ROW */}
@@ -139,13 +136,13 @@ const userAvatar =
 
             <div className="flex gap-5">
               {games.map((game, i) => (
-               <Link key={i} href={`/games/${game.name}`}>
+               <Link key={i} href={`/games/${game.gameName}`}>
                 <div className="w-44 cursor-pointer">
-                 <img
-                  src={game.img}
+                 <Image
+                  src={game.gameImg} alt={game.gameName} width={200} height={160}
                   className="rounded-xl mb-2 h-40 w-full object-cover transition duration-300 hover:scale-105 hover:shadow-[0_0_20px_rgba(79,70,229,0.4)]"
                  />
-                   <p className="text-sm font-semibold">{game.name}</p>
+                   <p className="text-sm font-semibold">{game.gameName}</p>
                    <div className="text-[#FFBF00] text-sm">★★★★★</div>
                </div>
              </Link>
@@ -197,26 +194,26 @@ const userAvatar =
                 key={i}
                 className="flex gap-3 bg-[#0F172A] p-3 rounded-lg mb-2"
               >
-                <img
-                  src={play.img}
+                <Image
+                  src={play.gameImg} alt={play.gameName} width={48} height={48}
                   className="w-12 h-12 rounded-lg object-cover"
                 />
 
                 <div className="flex-1">
-                  <p className="text-sm font-semibold">{play.name}</p>
+                  <p className="text-sm font-semibold">{play.gameName}</p>
                   <p className="text-xs text-gray-400">
-                    {play.date} | {play.players} Players | {play.time}
+                    {play.playDate} | {play.playerCount} Players | {play.playDuration}
                   </p>
                 </div>
 
                 <span
                   className={`text-sm font-semibold ${
-                    play.result === "Won"
+                    play.gameResult === "Won"
                       ? "text-[#10B981]"
                       : "text-[#EF4444]"
                   }`}
                 >
-                  {play.result}
+                  {play.gameResult}
                 </span>
               </div>
             ))}
@@ -241,8 +238,8 @@ const userAvatar =
                />
                 {/* Game Image */}
                 <Image
-                src={item.img}
-                alt={item.name}
+                src={item.gameImg}
+                alt={item.gameName}
                 width={50}
                height={50}
                className="rounded-lg object-cover"
@@ -250,20 +247,20 @@ const userAvatar =
 
                 <div className="flex flex-col">
                  <span className="text-sm font-semibold">
-                  {item.user} • {item.time}
+                  {item.user} • {item.timeAgo}
                  </span>
                  <span className="text-xs text-gray-400">
-                  Played {item.name}
+                  Played {item.gameName}
                   </span>
                </div>
                 {/* Result */}
-               {item.result && (
+               {item.gameResult && (
                <span
                className={`ml-auto text-xs px-2 py-1 rounded font-semibold ${
-               item.result === "Won" ? "bg-green-600 text-white" : "bg-red-600 text-white"
+               item.gameResult === "Won" ? "bg-green-600 text-white" : "bg-red-600 text-white"
                }`}
                >
-               {item.result}
+               {item.gameResult}
               </span>
                )}
                 <span className="ml-auto text-xs bg-[#4F46E5] px-2 py-1 rounded">
@@ -273,74 +270,6 @@ const userAvatar =
             ))}
           </div>
         </div> 
-         {/* Collapsible analytic panel */}
-         <CollapsiblePanel title="View your analytics 📊">
-
-        {/* STATS */}
-         <div className="bg-[#1E293B]/70 p-5 rounded-2xl text-center shadow-lg">
-          <h2 className="mb-4">Your Stats</h2>
-
-          <div className="flex justify-around mb-6">
-         <div>
-        <p className="text-gray-400 text-sm">Total Plays</p>
-        <p className="text-2xl font-bold text-indigo-400">58</p>
-        <p className="text-xs text-green-400">+11 this month</p>
-          </div>
-
-         <div>
-         <p className="text-gray-400 text-sm">Win Rate</p>
-         <p className="text-2xl font-bold text-indigo-400">71%</p>
-         <p className="text-xs text-green-400">+6% improvement</p>
-         </div>
-
-         <div>
-         <p className="text-gray-400 text-sm">Owned</p>
-         <p className="text-2xl font-bold text-indigo-400">24</p>
-         <p className="text-xs text-green-400">+2 new</p>
-         </div>
-       </div>
-       </div>
-
-      {/* CHARTS */}
-       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
-
-       {/* WIN RATE */}
-       <div 
-          onClick={() => setOpenChart("winrate")}
-          className="bg-slate-800 p-4 rounded-xl h-[300px] flex flex-col cursor-pointer hover:scale-[1.02] transition"
-       >
-        <h2 className="mb-2 text-center">Win Rate</h2>
-
-       <div className="flex-1 flex items-center justify-center">
-        <WinRateChart className="w-full h-full" />
-        </div>
-       </div>
-
-       {/* DONUT */}
-       <div
-         onClick={() => setOpenChart("donut")}
-         className="bg-slate-800 p-4 rounded-xl h-[300px] flex flex-col items-center justify-center cursor-pointer hover:scale-[1.02] transition"
-        >
-        <h2 className="mb-3">Game Types</h2>
-
-        <div
-        className="w-40 h-40 rounded-full relative"
-        style={{
-          background:
-            "conic-gradient(#4F46E5 40%, #10B981 25%, #FFBF00 20%, #EF4444 15%)",
-        }}
-        >
-        <div className="absolute inset-6 bg-[#0F172A] rounded-full" />
-        </div>
-
-        <p className="text-gray-400 text-sm mt-2 text-center">
-        Strategy • Party • Co-op • Family
-        </p>
-        </div>
-
-       </div>
-
-       </CollapsiblePanel>
 
         {/* BOTTOM ROW */}
         <div className="grid grid-cols-2 gap-6 mt-6">
@@ -361,73 +290,6 @@ const userAvatar =
 
         </div>
       </div>
-      {openChart === "winrate" && (
-        <div className="fixed inset-0 bg-black/70 flex items-center justify-center z-50">
-    
-          <div className="bg-[#1E293B] p-6 rounded-2xl w-[600px] h-[450px] relative shadow-xl">
-
-          {/* CLOSE BUTTON */}
-          <button
-          onClick={() => setOpenChart(null)}
-          className="absolute top-3 right-3 bg-gray-700 px-3 py-1 rounded-lg hover:bg-gray-600 transition"
-        >
-          ✕
-         </button>
-
-         {/* TITLE */}
-          <h2 className="text-center mb-4 text-lg font-semibold">
-            Win Rate Details
-          </h2>
-
-         {/* BIG CHART */}
-          <div className="w-full h-[350px]">
-          <WinRateChart className="w-full h-full" />
-          </div>
-
-          </div>
-       </div>
-      )}
-      {openChart === "donut" && (
-      <div
-        onClick={() => setOpenChart(null)}
-        className="fixed inset-0 bg-black/70 flex items-center justify-center z-50"
-       >
-       <div
-         onClick={(e) => e.stopPropagation()}
-        className="bg-[#1E293B] p-6 rounded-2xl w-[600px] h-[450px] relative shadow-xl"
-       >
-       {/* CLOSE BUTTON */}
-        <button
-        onClick={() => setOpenChart(null)}
-        className="absolute top-3 right-3 bg-gray-700 px-3 py-1 rounded-lg hover:bg-gray-600 transition"
-        >
-        ✕
-        </button>
-
-        {/* TITLE */}
-       <h2 className="text-center mb-4 text-lg font-semibold">
-         Game Types Details
-        </h2>
-
-        {/* BIG DONUT */}
-        <div className="w-full h-[350px] flex items-center justify-center">
-         <div
-          className="w-64 h-64 rounded-full relative"
-          style={{
-            background:
-              "conic-gradient(#4F46E5 40%, #10B981 25%, #FFBF00 20%, #EF4444 15%)",
-          }}
-        >
-          <div className="absolute inset-12 bg-[#0F172A] rounded-full" />
-        </div>
-      </div>
-
-       <p className="text-gray-400 text-sm mt-2 text-center">
-        Strategy • Party • Co-op • Family
-        </p>
-       </div>
-       </div>
-       )}
-      </div>
+    </div>
   );
 }
