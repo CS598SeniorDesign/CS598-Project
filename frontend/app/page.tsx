9cb43760a611495b8ab8901607762d78ef8cc162
Supriya gurung <grgsupriyaa890@gmail.com>
2026-09-24 03:09:50 -0500

    Fix frontend formatting for CI

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,6 @@
 export default function HomePage() {
   return (
     <section className="min-h-screen flex flex-col items-center justify-center bg-background text-white px-6">
-      
       <h1 className="text-7xl md:text-8xl font-extrabold tracking-light mb-6 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent">
         Game just got competitive!?
       </h1>
@@ -11,19 +10,14 @@ export default function HomePage() {
       </p>
 
       <div className="flex gap-4">
-        <button className="bg-primary px-6 py-3 rounded-lg">
-          Get Started
-        </button>
+        <button className="bg-primary px-6 py-3 rounded-lg">Get Started</button>
 
         <button className="border border-gray-700 px-6 py-3 rounded-lg">
           Learn More
         </button>
       </div>
-
     </section>
-  )
+  );
 }
 
-<div className="bg-red-500 text-white p-10 mt-20">
-  Tailwind Test
-</div>
+<div className="bg-red-500 text-white p-10 mt-20">Tailwind Test</div>;
