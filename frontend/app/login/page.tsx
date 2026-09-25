62579b111d05ce30d10f461ed93b1b74ad23c8bf
huyres1 <168512225+huyres1@users.noreply.github.com>
2026-09-25 09:52:01 -0500

    Update page.tsx

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { responseCookiesToRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
 import { useRouter } from "next/navigation";
 import { useState, type FormEvent } from "react";
 
